66a8b116614193534d849b81d3fb39fbd3134b2b
Denys Meloshyn <ned1988@gmail.com>
2017-01-30 20:10:07 +0100

    Apply timestamp to user update request

--- user_update_resource.py
+++ user_update_resource.py
@@ -12,7 +12,7 @@ from credentials_validator import CredentialsValidator
 
 def get_parameters(parser):
     parser.add_argument(Constants.k_time_stamp, type=inputs.iso8601interval, help='Time stamp date (ISO 8601)',
-                        location='headers', required=True)
+                        location='headers')
     parser.add_argument(Constants.k_user_id, type=int, help='User ID', location='headers', required=True)
     parser.add_argument(Constants.k_token, type=str, help='User token', location='headers', required=True)
 
@@ -34,16 +34,17 @@ class UserUpdateResource(Resource):
         status, message = CredentialsValidator.is_user_credentials_valid(user_id, token)
 
         if status is False:
-            return message
-
-        time_stamp = args.get(Constants.k_time_stamp)
-        if type(time_stamp) is tuple:
-            time_stamp = time_stamp[0]
-        else:
-            return Constants.error_reponse('wrong_time_stamp')
+            return message, 401
 
         subquery = db.session.query(UserGroup.group_id).filter(UserGroup.user_id == user_id).subquery()
-        query = db.session.query(User).filter(User.user_id == UserGroup.user_id, UserGroup.group_id.in_(subquery))
-        items = [model.to_json() for model in query.filter().all()]
+        query = db.and_(User.user_id == UserGroup.user_id, UserGroup.group_id.in_(subquery))
 
-        return Constants.default_response(items)
+        time_stamp = args.get(Constants.k_time_stamp)
+        if time_stamp is not None:
+            time_stamp = time_stamp[0]
+            items = db.session.query(User).filter(query, User.time_stamp > time_stamp)
+        else:
+            items = db.session.query(User).filter(query)
+        result = [model.to_json() for model in items.filter().all()]
+
+        return Constants.default_response(result)
