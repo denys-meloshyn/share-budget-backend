fe322681b1e8c6239299e615cd167efbe988f1f4
Denys Meloshyn <ned1988@gmail.com>
2017-02-18 21:37:58 +0100

    Change location parameters from body to headers

--- expense_resource.py
+++ expense_resource.py
@@ -12,14 +12,14 @@ from credentials_validator import CredentialsValidator
 def put_parameters(parser):
     parser.add_argument(Constants.k_expense_id, type=int, help='Expense ID (if empty new expense will be created)',
                         location='form')
-    parser.add_argument(Constants.k_category_id, type=int, help='Category ID', location='form', required=True)
-    parser.add_argument(Constants.k_group_id, type=int, help='Group ID', location='form', required=True)
-    parser.add_argument(Constants.k_name, type=str, help='Expense name', location='form', required=True)
-    parser.add_argument(Constants.k_price, type=float, help='Expense price', location='form', required=True)
+    parser.add_argument(Constants.k_category_id, type=int, help='Category ID', location='headers')
+    parser.add_argument(Constants.k_group_id, type=int, help='Group ID', location='headers', required=True)
+    parser.add_argument(Constants.k_name, type=str, help='Expense name', location='headers', required=True)
+    parser.add_argument(Constants.k_price, type=float, help='Expense price', location='headers', required=True)
 
-    parser.add_argument(Constants.k_user_id, type=int, help='User ID', location='form', required=True)
-    parser.add_argument(Constants.k_token, type=str, help='User token', location='form', required=True)
-    parser.add_argument(Constants.k_is_removed, type=inputs.boolean, help='Is group limit removed', location='form')
+    parser.add_argument(Constants.k_user_id, type=int, help='User ID', location='headers', required=True)
+    parser.add_argument(Constants.k_token, type=str, help='User token', location='headers', required=True)
+    parser.add_argument(Constants.k_is_removed, type=inputs.boolean, help='Is group limit removed', location='headers')
     parser.add_argument(Constants.k_internal_id, type=int, help='Internal ID', location='headers')
 
 
