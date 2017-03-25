bff5a966f544bd66600d5f17c42f538402897069
Denys Meloshyn <ned1988@gmail.com>
2017-03-25 11:21:39 +0100

    Send registration email

--- registration_email.py
+++ registration_email.py
@@ -3,6 +3,15 @@ from flask_mail import Message
 from constants import Constants
 from shared_objects import mail
 
+#
+#
+
+# 		<p>If you did not register for Share Budget, just ignore this message.</p>
+# 		<p>With any quetions contact
+# 			<a href="mailto:sharebudgetproject@gmail.com?Subject=Remarks and offers">sharebudgetproject@gmail.com</a>.
+# 		</p>
+# 		<p>Best regards,
+# 		<br>Share Budget Team</p>
 
 def send_registration_email(user):
     msg = Message()
@@ -10,17 +19,22 @@ def send_registration_email(user):
     msg.subject = 'Share Budget Registration Completion Information for ' + user.first_name
     msg.recipients = [user.email]
 
-    msg.body = 'Hi ' + user.first_name + ',\n\n'
-    msg.body += 'Thanks for joining Share Budget. To complete your registration, please click the link below to ' \
-                'approve your email:\n\n'
-    msg.body += 'https://sharebudget.herokuapp.com'
-    msg.body += Constants.k_registration_resource_path
-    msg.body += '?'
-    msg.body += Constants.k_token
-    msg.body += '='
-    msg.body += user.registration_email_token + '\n\n'
-    msg.body += 'If you did not register for Shared Budget, please disregard this message.\n'
-    msg.body += 'Please contact ' + Constants.project_email + 'with any questions.\n\n'
-    msg.body += 'Shared Budget Team'
+    msg.html = '<html>'
+    msg.html += '<body>'
+    msg.html += '<p>Hi ' + user.first_name + '!</p>'
+    msg.html += '<p>Thanks for joining Share Budget. To complete your registration, please, click the following link to approve your email:</p>'
+    msg.html += '<p> <a href = "'
+    msg.html += 'https://sharebudget.herokuapp.com'
+    msg.html += Constants.k_registration_resource_path
+    msg.html += '?'
+    msg.html += Constants.k_token
+    msg.html += '='
+    msg.html += user.registration_email_token
+    msg.html += '">Complete registration</a></p>'
+    msg.html += '<p>If you did not register for Share Budget, just ignore this message.</p>'
+    msg.html += '<p>With any quetions contact <a href="mailto:sharebudgetproject@gmail.com?Subject=Remarks and offers">sharebudgetproject@gmail.com</a>.</p>'
+    msg.html += '<p>Best regards, <br>Share Budget Team</p>'
+    msg.html += '</body>'
+    msg.html += '</html>'
 
     mail.send(msg)
