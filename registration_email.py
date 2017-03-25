b08e1b5430e54131e93e4864732d18526533f8b7
Denys Meloshyn <ned1988@gmail.com>
2017-03-25 18:22:43 +0100

    Email message

--- registration_email.py
+++ registration_email.py
@@ -3,15 +3,6 @@ from flask_mail import Message
 from constants import Constants
 from shared_objects import mail
 
-#
-#
-
-# 		<p>If you did not register for Share Budget, just ignore this message.</p>
-# 		<p>With any quetions contact
-# 			<a href="mailto:sharebudgetproject@gmail.com?Subject=Remarks and offers">sharebudgetproject@gmail.com</a>.
-# 		</p>
-# 		<p>Best regards,
-# 		<br>Share Budget Team</p>
 
 def send_registration_email(user):
     msg = Message()
@@ -22,7 +13,8 @@ def send_registration_email(user):
     msg.html = '<html>'
     msg.html += '<body>'
     msg.html += '<p>Hi ' + user.first_name + '!</p>'
-    msg.html += '<p>Thanks for joining Share Budget. To complete your registration, please, click the following link to approve your email:</p>'
+    msg.html += '<p>Thanks for joining Share Budget. To complete your registration, please, click the following ' \
+                'link to approve your email:</p>'
     msg.html += '<p> <a href = "'
     msg.html += 'https://sharebudget.herokuapp.com'
     msg.html += Constants.k_registration_resource_path
@@ -32,7 +24,8 @@ def send_registration_email(user):
     msg.html += user.registration_email_token
     msg.html += '">Complete registration</a></p>'
     msg.html += '<p>If you did not register for Share Budget, just ignore this message.</p>'
-    msg.html += '<p>With any quetions contact <a href="mailto:sharebudgetproject@gmail.com?Subject=Remarks and offers">sharebudgetproject@gmail.com</a>.</p>'
+    msg.html += '<p>With any quetions contact <a href="mailto:sharebudgetproject@gmail.com?Subject=Remarks' \
+                ' and offers">sharebudgetproject@gmail.com</a>.</p>'
     msg.html += '<p>Best regards, <br>Share Budget Team</p>'
     msg.html += '</body>'
     msg.html += '</html>'
