645ffa27549d7a06612fe9ccb28fc738f0a73ecd
Denys Meloshyn <denys.meloshyn@gmail.com>
2019-12-30 13:58:01 +0100

    development: Add Apple universal link association file

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+
+from flask import send_from_directory
+
 from application import create_app
 from model import db
 
@@ -5,6 +9,13 @@ flask_app = create_app()
 flask_app.app_context().push()
 db.create_all()
 db.session.commit()
+
+
+@flask_app.route('/.well-known/<path:path>')
+def send_well_known(path):
+    root_dir = os.getcwd()
+    return send_from_directory(os.path.join(root_dir, '.well-known'), path)
+
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
