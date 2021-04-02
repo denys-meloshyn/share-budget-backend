5c6ca4ad9b95651abe89758f4da2b9c7c9622b9e
Denys Meloshyn <denys.meloshyn@gmail.com>
2021-04-02 16:01:48 +0200

    [feature/total-expenses] Update totals for expenses

--- model/group_total_expenses.py
+++ model/group_total_expenses.py
@@ -5,16 +5,20 @@ from utility.constants import Constants
 class GroupTotalExpenses(db.Model):
     __tablename__ = 'GROUP_TOTAL_EXPENSES'
 
-    group_total_expenses_id = db.Column(db.Integer, primary_key=True)
+    period_date = db.Column(db.String, primary_key=True)
     group_id = db.Column(db.Integer, db.ForeignKey('GROUP.group_id'))
+    total = db.Column(db.Float)
     is_removed = db.Column(db.Boolean)
     time_stamp = db.Column(db.DateTime)
 
     def __init__(self):
+        self.total = 0
         self.is_removed = False
 
     def to_json(self):
         json_object = {
+            Constants.JSON.period_date: self.period_date,
+            Constants.JSON.total: self.total,
             Constants.JSON.group_id: self.group_id,
             Constants.JSON.is_removed: self.is_removed
         }
